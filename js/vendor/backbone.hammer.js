31dbd7a928f251c0f61e9890a7ab01cc3ce5a482
joshvillahermosa <joshvillahermosa@gmail.com>
2014-03-12 16:20:01 -0500

    NOTE: Proven code from wookiehangover that backbone.hammer.js works

--- js/vendor/backbone.hammer.js
+++ js/vendor/backbone.hammer.js
@@ -1,4 +1,7 @@
-(function(root, factory) {
+/*
+  What was included in the code from WookieHangover
+*/
+/*(function(root, factory) {
   // Set up Backbone appropriately for the environment.
   if (typeof define === 'function' && define.amd) {
     // AMD
@@ -77,4 +80,42 @@
       return this.$el.hammer(options);
     }
   });
-}));
+}));*/
+
+
+/*Second code from WookieHangover*/
+//THIS CODE HAS BEEN THE PROVEN SOLUTIOON FOR IT TO WORK
+
+(function(root){
+  var Backbone = root.Backbone;
+  var _ = root._;
+  var $ = Backbone.$;
+ 
+  if( !Backbone || !$.fn.hammer ){
+    return;
+  }
+ 
+  var delegateEvents = Backbone.View.prototype.delegateEvents;
+  var undelegateEvents = Backbone.View.prototype.undelegateEvents;
+ 
+  Backbone.View.prototype.delegateEvents = function(){
+    var events = this.hammerEvents;
+    if( events ){
+      var options = this.hammerOptions || {};
+      var hammerContext = _.extend({}, this, {
+        $el: this.hammer(options)
+      });
+      delegateEvents.call(hammerContext, events);
+    }
+    return delegateEvents.apply(this, arguments);
+  };
+ 
+  Backbone.View.prototype.undelegateEvents = function(){
+    this.hammer().off('.delegateEvents' + this.cid);
+    return undelegateEvents.apply(this, arguments);
+  };
+ 
+  Backbone.View.prototype.hammer = function(options){
+    return this.$el.hammer(options);
+  };
+})(window);
